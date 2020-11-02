a9eef42d9b2b805f0f09251923ffbde48185232b
venatiodecorus <opensourcekeys@gmail.com>
2020-11-01 20:58:22 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# thelounge-plugin-exec
+
+Recreates irssi's `/exec` command for [thelounge](https://thelounge.chat), which allows you to run arbitrary commands on your host and send the output to IRC. Useful for playing ASCII art, cowsay, etc. Thank you to MiniDigger's shortcuts [plugin](https://github.com/minidigger/thelounge-plugin-shortcuts) for the inspiration and guide.
+
+# Installation
+
+- If you have installed thelounge via NPM/Yarn:
+
+   `thelounge install thelounge-plugin-shortcuts`
+- If you have installed thelounge via source:
+
+   `node index.js install thelounge-plugin-shortcuts`
+
+# Usage
+
+Just run `/exec <command>` to run a command on your host. Obviously this plugin is only recommended for private instances. If you want to use it on a public instance please make sure your permissions are set properly so users can't run harmful commands.
+
+If a command is running too long you can use `/exec --stop`.
+
+To set the flood protection rates use `/exec --set-rate <burst> <per-second>` where the first value is a number and the burst flood rate (number of commands to send instantly) and the second value is the delay in milliseconds between messages after that. These default to a value of 5 for the burst and a value of 200 for per-second.
---
+++ index.js
@@ -0,0 +1,77 @@
+"use strict";
+
+const Msg = require("thelounge/src/models/msg");
+const log = require("thelounge/src/log");
+const Helper = require("thelounge/src/helper");
+const Utils = require("thelounge/src/command-line/utils");
+const fs = require("fs");
+const path = require('path');
+const { exec } = require("child_process");
+
+let thelounge = null;
+let stop = false;
+let burst = 5;
+let perSec = 200;
+
+const execCommand = {
+    input: function (client, target, command, args) {
+        if (args.length === 0) {
+            client.sendMessage(red + "Usage: /exec <command|--stop|--set-rate <burst> <per-sec>>", target.chan);
+            return;
+        }
+
+        switch(args[0]) {
+            case "--stop":
+                stop = true;
+                return
+            break;
+            case "--set-rate":
+                burst = args[1];
+                perSec = args[2];
+                break;
+            default:
+                exec(args.join(' '), (error, stdout, stderr) => {
+                    if(error) {
+                        sendErrorMessage(`error: ${error.message}`, target.chan, client);
+                        return
+                    }
+                    if(stderr) {
+                        sendErrorMessage(`stderr: ${stderr}`, target.chan, client);
+                        return;
+                    }
+
+                    // Get first burst amount of lines
+                    let burstText = stdout.trim().split(/[\r?\n]+/, burst);
+                    for(var i = 0; i < burst; i++) {
+                        client.runAsUser(burstText[i], target.chan.id);
+                    }
+        
+                    let restText = stdout.trim().split(/[\r?\n]+/);
+                    restText = restText.splice(burst, restText.length-1);
+                    var it = 0;
+                    var intObj = setInterval(() => {
+                        client.runAsUser(restText[it++], target.chan.id);
+                        if(stop || it == restText.length) {
+                            stop = false;
+                            it = 0;
+                            clearInterval(intObj);
+                        }
+                    }, perSec)
+                });
+            break;
+        }
+    },
+    allowDisconnected: true
+};
+
+module.exports = {
+    onServerStart: api => {
+        thelounge = api;
+        thelounge.Commands.add("exec", execCommand);
+        // if (!fs.existsSync(shortcutsFile)) {
+        //     log.warn("Shortcut file " + shortcutsFile + " doesn't exist. Creating...")
+        //     saveShortcuts();
+        // }
+        // loadShortcuts();
+    },
+};
